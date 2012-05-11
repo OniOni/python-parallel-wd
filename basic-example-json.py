a985e7c2b9dc71bdb4bbe3231990eeaa420b4a7e
msabourin <mathieu.c.sabourin@gmail.com>
2012-05-11 11:53:46 -0700

    Example with configuration as a json file.

---
+++ basic-example-json.py
@@ -0,0 +1,34 @@
+import unittest
+from selenium import webdriver
+import parallelWd
+import copy
+
+class Selenium2OnSauce(unittest.TestCase):
+
+    def setUp(self):
+        self.drivers = parallelWd.Remote()
+        self.drivers.load_config_file("config.json")
+        
+
+    @parallelWd.multiply
+    def test_sauce(self):
+        self.driver.get('http://saucelabs.com/test/guinea-pig')
+        self.assertTrue("I am a page title - Sauce Labs" in self.driver.title);
+        self.driver.find_element_by_id('comments').send_keys('Hello! I am some example comments. I should appear in the page after you submit the form')
+        self.driver.find_element_by_id('submit').click()
+
+        comments = self.driver.find_element_by_id('your_comments')
+        self.assertTrue('Your comments: Hello! I am some example comments. I should appear in the page after you submit the form' in comments.text)
+        body = self.driver.find_element_by_xpath('//body')
+        self.assertFalse('I am some other page content' in body.text)
+        self.driver.find_elements_by_link_text('i am a link')[0].click()
+        body = self.driver.find_element_by_xpath('//body')
+        self.assertTrue('I am some other page content' in body.text)
+
+    @parallelWd.multiply
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
+
