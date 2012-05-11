be3ac3fcf380da75c4f1cf43d8a817dd580b8de7
msabourin <mathieu.c.sabourin@gmail.com>
2012-05-10 18:14:07 -0700

    First working version of parallelWd.

---
+++ parallelWd.py
@@ -0,0 +1,56 @@
+from selenium import webdriver 
+import unittest
+
+class Remote(object):
+    """
+    """
+    
+    def __init__(self, desired_capabilities, command_executor):
+        """
+        
+        Arguments:
+        - `desired_capabilities`: Array of desired_capabilities
+        - `command_executor`: Id string
+        """
+        
+        self._command_executor = command_executor
+        self._drivers = []
+
+        #Set up all webdrivers
+        self.__create_drivers(desired_capabilities)
+        
+    def load_config_file(self, file):
+        """Open json file and load config from it
+        
+        Arguments:
+        - `file`: json file containing conf
+        """
+
+    def __create_drivers(self, desired_capabilities):
+        """Create  webdrives from desired capabilities
+        
+        Arguments:
+        - `self`:
+        - `desired_capabilities`:
+        """
+        for d in desired_capabilities:
+            self._drivers += [webdriver.Remote(desired_capabilities=d,
+                                               command_executor=self._command_executor)]
+
+            
+            
+def multiply(test):
+    """Make test run in mutiple browsers
+    """
+
+    class SubTest(unittest.TestCase):
+        def __init__(self, driver=None):
+            self.driver = driver
+
+    def wrapper(*args, **kwargs):
+        for d in args[0].drivers._drivers:
+            o = SubTest(d)
+            test(o)
+            
+    return wrapper
+
