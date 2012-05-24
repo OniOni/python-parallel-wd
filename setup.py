205960da74167802129f9fc0ccea1eddfcd605f0
msabourin <mathieu.c.sabourin@gmail.com>
2012-05-23 17:28:02 -0700

    Correct setup file with dependencies.

--- setup.py
+++ setup.py
@@ -11,14 +11,16 @@ def read(fname):
 setup(
     name = "python-wd-parallel",
     packages = ['wd'],
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Mathieu Sabourin",
     author_email = "mathieu.c.sabourin@gmail.com",
     maintainer = "Mathieu Sabourin",
     maintainer_email = "mathieu.c.sabourin@gmail.com",
     description = ("python-wd-parallel lets you easilly run your test in multiple borwsers."),
     keywords = " Testing, Selenium",
-    url = " https://github.com/OniOni/python-parallel-wd",
+    url = "https://github.com/OniOni",
+    download_url = "https://github.com/OniOni/python-parallel-wd",
+    install_requires = ["selenium>=2.21.3"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: No Input/Output (Daemon)",
