05ac0c575613a72db87faf7a583ad8ee1854eab5
msabourin <mathieu.c.sabourin@gmail.com>
2012-05-23 17:33:09 -0700

    Now really parallel.
    For each test case, a process is run for each configuration.

--- wd/parallel.py
+++ wd/parallel.py
@@ -1,6 +1,7 @@
 from selenium import webdriver 
 import unittest
 import json
+import multiprocessing
 
 class Remote(object):
     """
@@ -15,11 +16,12 @@ class Remote(object):
         """
 
         self._command_executor = command_executor
-        self._drivers = []
 
         #Set up all webdrivers
         if desired_capabilities != None and command_executor != None:
-            self.__create_drivers(desired_capabilities)
+        #    self.__create_drivers(desired_capabilities)
+            self._desired_capabilities = desired_capabilities
+        
         
     def load_config_file(self, file):
         """Open json file and load config from it
@@ -31,7 +33,8 @@ class Remote(object):
         conf = json.load(fd)
         self._command_executor = self.__build_command_executor(conf['remote'])
 
-        self.__create_drivers(conf['desired'])
+        #self.__create_drivers(conf['desired'])
+        self._desired_capabilities = conf['desired']
 
     def __build_command_executor(self, remote):
         return str('http://'+remote['username']+':'+remote['accessKey']+'@'+\
@@ -47,7 +50,13 @@ class Remote(object):
         for d in desired_capabilities:
             self._drivers += [webdriver.Remote(desired_capabilities=d,
                                                command_executor=self._command_executor)]
+    def register(self, wd):
+        try:
+            self._drivers += [wd]
+        except AttributeError as e:
+            self._drivers = [wd]
 
+        print repr(len(self._drivers)) + ' drivers registered'
             
             
 def multiply(test):
@@ -57,11 +66,58 @@ def multiply(test):
     class SubTest(unittest.TestCase):
         def __init__(self, driver=None):
             self.driver = driver
+            self.driver.implicitly_wait(30)
 
+    def thread_func(f, dc=None, ce=None, driver=None, queue=None):
+
+        if driver == None and dc != None:
+            driver = webdriver.Remote(desired_capabilities=dc,
+                                      command_executor=ce)
+            if queue != None:
+                queue.put(driver)
+
+        print 'Launching test'
+        f(SubTest(driver))
+        
     def wrapper(*args, **kwargs):
-        for d in args[0].drivers._drivers:
-            o = SubTest(d)
-            test(o)
+        threads = []
+        queue = multiprocessing.Queue(len(args[0].drivers._desired_capabilities) + 1)
+        i = 0
+        nb_d = 0
+
+        try:
+            print repr(len(args[0].drivers._drivers)) + ' drivers.'
+            for d in args[0].drivers._drivers:
+                t = multiprocessing.Process(target=thread_func, args=(test,), kwargs={'driver': d})
+                t.start()
+                threads += [t]
+                i += 1
+                print "One thread started | " +repr(i)+ " threads running"
+                
+        except AttributeError:
+            for c in args[0].drivers._desired_capabilities:
+                t = multiprocessing.Process(target=thread_func, args=(test,),
+                                            kwargs={'dc': c,
+                                             'ce': args[0].drivers._command_executor,
+                                             'queue': queue
+                                                })
+                t.start()
+                threads += [t]
+                i += 1
+                print "One thread started | " +repr(i)+ " threads running"
+
+            while nb_d < len(args[0].drivers._desired_capabilities):
+                driver = queue.get(block=True)
+                args[0].drivers.register(driver)
+                nb_d += 1
+
+        for t in threads:
+            t.join()
+            i -= 1
+            print "One thread stopped | " +repr(i)+ " threads running"
+            
+        # o = SubTest(d)
+        # test(o)
             
     return wrapper
 
