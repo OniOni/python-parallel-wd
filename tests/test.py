0e8789e8d81de303d99b4951dbc236a6b003d431
OniOni <mathieu.c.sabourin@gmail.com>
2013-11-10 17:53:24 -0800

    Add skeleton test case for travis integration.

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+import unittest
+import wd
+
+class TestValideUnitTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_dummy(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
